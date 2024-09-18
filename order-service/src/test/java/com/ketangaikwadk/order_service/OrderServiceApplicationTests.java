1ba6a3ff4561130e6f41f7baedf25fee620e887d
Ketan <ketangaikwadk@gmail.com>
2024-09-18 19:40:02 +0530

    created order service starter

---
+++ order-service/src/test/java/com/ketangaikwadk/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketangaikwadk.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
