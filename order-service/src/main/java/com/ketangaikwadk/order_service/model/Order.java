0cd745ce1acd3eea75d28c9005de542e1c6ab03a
Ketan <ketangaikwadk@gmail.com>
2024-09-18 19:53:51 +0530

    created models for order service

---
+++ order-service/src/main/java/com/ketangaikwadk/order_service/model/Order.java
@@ -0,0 +1,28 @@
+package com.ketangaikwadk.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
